f7e7158c251764ba1269ffd4f391e25fcf956959
Matthew Tucker <matthewt263@gmail.com>
2018-07-29 14:38:35 -0700

    changed index.html

--- src/App.js
+++ src/App.js
@@ -3,32 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state= {
-
-  }
+  // state= {
+  //
+  // }
 
   onFetch() {
     console.log('fetch calling');
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.searchBox},California&appid=0de82b6b4ba5d843dac44bbee4d02543`)
+    fetch("data/data.json")
     .then(response => response.json())
     .then(data => {
       console.log('got data:', data);
-      this.setState({
-
-      });
+      // this.setState({
+      //
+      // });
     }
   }
 
+  onFetch();
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="Graph">
+        <div class="Graph-header">
+  		     <h1>San Francisco Weather Over the Last 10 Years</h1>
+        </div>
+
+        <div class="Graph-container">
+          <p>All records taken on July 15th</p>
+  				<button onCLick="highTemp()">Highest temp</button>
+  				<button onCLick="lowTemp()">Lowest temp</button>
+          <div class="Graph-bars">
+          </div>
+        </div>
       </div>
     );
   }
