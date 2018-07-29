4b0ce26b9dbe96d3acdffca3d6121431aa9e1605
Matthew Tucker <matthewt263@gmail.com>
2018-07-29 13:57:40 -0700

    added readme and some JS code

---
+++ README.md
@@ -0,0 +1,5 @@
+#Third Iteration of Graph
+
+1. Data comes from data.json, originally from weather underground
+
+2. Converted old javascript into React and JSX
--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state= {
+
+  }
+
+  onFetch() {
+    console.log('fetch calling');
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.searchBox},California&appid=0de82b6b4ba5d843dac44bbee4d02543`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('got data:', data);
+      this.setState({
+
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
